cf519ecf776ab0cd77f74f031e7dc0d189f6b693
tonhus <antonio_abrantes@hotmail.com.br>
2019-10-25 10:20:30 -0300

    conexao com banco

--- temp.js
+++ temp.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dbroot:plokiju78@crud-node-bpm2g.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dbroot:<senha>@crud-node-bpm2g.mongodb.net/test?retryWrites=true&w=majority";
 
 MongoClient.connect(uri, (err, client) =>{
     if(err) return console.log(err)
@@ -19,3 +19,12 @@ app.post('/show', (req, res)=>{
         res.redirect('/');
     })
 });
+
+/** 
+https://medium.com/baixada-nerd/criando-um-crud-completo-com-nodejs-express-e-mongodb-parte-1-3-6c8389d7147d
+
+https://medium.com/baixada-nerd/criando-um-crud-completo-com-nodejs-express-e-mongodb-parte-2-3-220a127d586f
+
+https://medium.com/baixada-nerd/criando-um-crud-completo-com-nodejs-express-e-mongodb-parte-3-3-b243d14a403c 
+
+*/
